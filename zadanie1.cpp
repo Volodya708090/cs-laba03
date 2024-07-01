3a582c4549582e181c4074f7e50327ce033c306f
Bob <bob@example.com>
2024-07-01 23:12:35 +0300

    code: добавлен вывод гистограммы как изображения в формате SVG

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 #include <iomanip>
 
 using namespace std;
@@ -25,15 +27,54 @@ vector<size_t> make_histogram(const vector<size_t>& numbers) {
     return bin_counts;
 }
 
-// Функция для отображения гистограммы
-void show_histogram_text(const vector<size_t>& bin_counts) {
-    for (size_t count : bin_counts) {
-        cout << setw(3) << count << "|";
-        for (size_t i = 0; i < count; ++i) {
-            cout << '*';
-        }
-        cout << endl;
+// Функция для отображения заголовка SVG
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+// Функция для отображения окончания SVG
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+// Функция для вывода текста в SVG
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+}
+
+// Функция для вывода прямоугольника в SVG
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
+}
+
+// Функция для отображения гистограммы в формате SVG
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    const double max_bin = *max_element(bins.begin(), bins.end());
+    const double scaling_factor = (IMAGE_WIDTH - TEXT_WIDTH) / max_bin;
+
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = bin * scaling_factor;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+        top += BIN_HEIGHT;
     }
+    svg_end();
 }
 
 int main() {
@@ -47,8 +88,8 @@ int main() {
     // Создание гистограммы
     vector<size_t> bin_counts = make_histogram(bins);
 
-    // Отображение гистограммы
-    show_histogram_text(bin_counts);
+    // Отображение гистограммы в формате SVG
+    show_histogram_svg(bin_counts);
 
     return 0;
 }
